a66e6104cf8234e700c779f851913d4c5452c0d2
stvalentin <stanciutheone@gmail.com>
2016-11-27 18:45:52 +0200

    Added some extra functionality

--- skmenunode/Classes/SKMenuNode.swift
+++ skmenunode/Classes/SKMenuNode.swift
@@ -10,99 +10,213 @@ import Foundation
 
 import SpriteKit
 
-protocol MenuNodeDelegate {
-    func menuTargetTouched(index: Int)
+public protocol MenuNodeDelegate  {
+    //func menuTargetTouched(index: Int)
+    func menuTargetTouched(_ index: Int, section: String);
 }
 
-public class SKMenuImage {
-
+open class SKMenuImage {
+    
 }
 
-public class SKMenuNode: SKNode {
+
+open class SKMenuPOP {
+    
+}
+
+public typealias callbackCompletion =  (() -> Void);
+
+struct MenuItem {
+    var item: AnyObject
+    let callback: callbackCompletion?
+}
+
+
+open class SKMenuNode: SKNode {
     
     var delegate: MenuNodeDelegate?
     
-    var padding:Int = 30;
-    var xPos:Int = 60;
+    var _padding:Int = 50;
+    var xPos:Int = 0;
     var yPos:Int = 0;
     
     var defaultText: SKLabelNode?;
     
-    var items:Array<AnyObject>
+    var items:Array<MenuItem>
+    
     var actions:Array<AnyObject>
     
-    override init () {
+    var layoutType: SKMenuLayout;
+    
+    var section: String = "default";
+    
+    fileprivate var _backgroundButton: UIColor = UIColor.blue;
+    
+    override public init () {
         items = Array();
         actions = Array();
-        
-        super.init()
-        self.userInteractionEnabled = true;
-        
+        layoutType = .horizontal;
+        super.init();
+        self.isUserInteractionEnabled = true;
     }
     
-    convenience init(delegate: MenuNodeDelegate) {
+    convenience public init(delegate: MenuNodeDelegate) {
         self.init();
         self.delegate = delegate
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    convenience public init(delegate: MenuNodeDelegate, section: String) {
+        self.init();
+        self.section = section;
+        self.delegate = delegate
+    }
+    
+    open func setBackgroundButton(color: UIColor) {
+        self._backgroundButton = color;
+    }
+    
+    open func setLayout(_ layoutType: SKMenuLayout) {
+        self.layoutType = layoutType;
+    }
+    
+    
+    open func setPadding(_ padding: Int = 20) {
+        self._padding = padding;
+    }
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             //let touch:UITouch = touches.anyObject() as! UITouch
-            let touchLocation = touch.locationInNode(self)
-            
+            let touchLocation = touch.location(in: self)
             for index in 0...items.count-1 {
-                if (CGRectContainsPoint(items[index].frame, touchLocation)) {
-                    delegate?.menuTargetTouched(index)
+                if (items[index].item.calculateAccumulatedFrame().contains(touchLocation)) {
+                    if (items[index].callback != nil) {
+                        items[index].callback!();
+                    } else{
+                        delegate?.menuTargetTouched(index, section: section);
+                    }
                 }
             }
         }
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func addChainTarget(item: String) -> AnyObject {
+    
+    @discardableResult
+    open func addChainTarget(_ item: String, callback: callbackCompletion? = nil) -> AnyObject {
+        let node:SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue");
+        //let layer = SKSpriteNode.init(color: UIColor.blueColor(), size: CGSize(width: 175, height: 45));
+        let layer = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 200, height: 45), cornerRadius: 12);
         
-        let node:SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue-UltraLight");
-        node.fontColor = UIColor.blackColor();
+        //ACTIVE /HOVER /DISABLED
+        
+        layer.strokeColor = _backgroundButton;
+        layer.fillColor = _backgroundButton;
+        
+        node.fontColor = UIColor.white;
         node.fontSize = 28;
         node.text = item;
-        node.color = UIColor.whiteColor()
+        node.color = UIColor.white
+        //node.anchorPoint = CGPointMake(0.5, 0.5);
+        node.position = CGPoint(x: layer.frame.midX , y: layer.frame.midY);
+        node.name = "text";
         
-        self.addChild(node);
-        items.append(node);
-        //actions.append(action);
-        node.position = CGPoint(x:0, y:xPos);
+        node.horizontalAlignmentMode = .center;
+        node.verticalAlignmentMode = .center;
+        layer.position = self.getPosition(layer);
         
-        xPos = xPos - (Int(node.frame.height) + self.padding);
+        layer.addChild(node);
+        layer.isUserInteractionEnabled = false;
+        self.addChild(layer);
+        items.append(MenuItem(item: layer, callback: callback));
         return self;
     }
     
     
-    
-    func addTarget(item: String) {
-        let node:SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue-UltraLight");
+    open func addTarget(_ item: String) {
+        let node:SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue");
         node.text = item
         node.fontSize = 28;
+        node.position = self.getPosition(node);
+        node.verticalAlignmentMode = .center;
+        node.horizontalAlignmentMode = .left;
+        
         self.addChild(node);
-        items.append(node);
-        //actions.append(action);
-        node.position = CGPoint(x:0, y:xPos);
-        xPos = xPos - (Int(node.frame.height) + self.padding);
+        items.append(MenuItem(item: node, callback: nil));
     }
     
-    func addText(item: String) {
-        defaultText  = SKLabelNode(fontNamed: "HelveticaNeue-UltraLight");
+    open func addTargetImage(_ item: SKSpriteNode, callback: callbackCompletion?) {
+        item.position = self.getPosition(item);
+        self.addChild(item);
+        items.append(MenuItem(item: item, callback: callback));
+    }
+    
+    open func changeTargetLabelContent(_ index: Int, content: String) {
+        let node = items[index].item as! SKShapeNode;
+        
+        node.children.forEach { (snode) in
+            let textNode = snode as! SKLabelNode;
+            textNode.text = content;
+        }
+    }
+    
+    
+    open func changeTargetContent(_ index: Int, content: String) {
+        let node = items[index].item as! SKLabelNode;
+        node.text = content;
+        
+        /*
+         let node = items[index] as! SKSpriteNode;
+         node.enumerateChildNodesWithName("name") { (node, unsafePointer) in
+         let nodeText = node as! SKLabelNode;
+         nodeText.text = content;
+         }
+         */
+    }
+    
+    
+    /**
+     * @TODO: Still al lot of pozitioning issues
+     */
+    func getPosition(_ node: SKNode) -> CGPoint {
+        
+        let padd = self.items.count != 0 ? self._padding: 5;
+        
+        if (self.layoutType == .horizontal) {
+            
+            if (self.items.count == 0) {
+                xPos =  (Int(node.calculateAccumulatedFrame().width) + padd);
+                return CGPoint(x: padd, y: 0);
+            }
+            
+            let xPosPrev = xPos + padd;
+            xPos +=  (Int(node.calculateAccumulatedFrame().width)) + padd;
+            
+            return CGPoint(x: xPosPrev, y: yPos);
+            
+        } else {
+            xPos = 0;
+            yPos -=  (Int(node.frame.height) + padd);
+        }
+        
+        return CGPoint(x: xPos, y: yPos);
+    }
+    
+    func addText(_ item: String) {
+        defaultText  = SKLabelNode(fontNamed: "HelveticaNeue");
         defaultText?.text = item
         
         self.addChild(defaultText!);
-        items.append(defaultText!);
+        
+        items.append(MenuItem(item: defaultText!, callback: nil));
         defaultText?.position = CGPoint(x:0, y:xPos);
-        xPos = xPos - (Int(defaultText!.frame.height) + self.padding);
+        xPos = xPos - (Int(defaultText!.frame.height) + self._padding);
     }
     
-    func changeText(item: String) {
+    func changeText(_ item: String) {
         defaultText?.text = item;
     }
     
@@ -112,11 +226,15 @@ public class SKMenuNode: SKNode {
     }
     
     func menuTargetTouched() {
-        
-        
+        //
     }
     
     func render() {
-        
+        //
     }
+}
+
+
+public enum SKMenuLayout {
+    case horizontal, vertical;
 }
